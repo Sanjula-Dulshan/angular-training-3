fbc180572df7d44a7100c2c3c1f2180ac362f482
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-06 00:11:24 +0530

    create course module and components

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -3,17 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AddStudentComponent } from './add-student/add-student.component';
 import { ShowStudentsComponent } from './show-students/show-students.component';
 import { EditStudentComponent } from './edit-student/edit-student.component';
-
-
+import { StudentRoutingModule } from './student-routing.module';
 
 @NgModule({
   declarations: [
     AddStudentComponent,
     ShowStudentsComponent,
-    EditStudentComponent
+    EditStudentComponent,
   ],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, StudentRoutingModule],
 })
-export class StudentModule { }
+export class StudentModule {}
