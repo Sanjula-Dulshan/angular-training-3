8029863b6d856bce30feb5454d109b13cfc0ae74
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-06 12:12:21 +0530

    add student router test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PrivateBaseComponent } from './private-base/private-base.component';
+import { AddStudentComponent } from './student/add-student/add-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'private',
+    component: PrivateBaseComponent,
+    children: [{ path: 'add-student', component: AddStudentComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/private-base/private-base.component.ts
+++ src/app/private-base/private-base.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-private-base',
   templateUrl: './private-base.component.html',
-  styleUrls: ['./private-base.component.scss']
+  styleUrls: ['./private-base.component.scss'],
 })
 export class PrivateBaseComponent {
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
+  goToAddStudent(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
+
+  goToShowStudent(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
+  goToEditStudent(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
+  goToAddCourse(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
+  goToShowCourse(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
+  goToEditCourse(): void {
+    this.router.navigate(['add-student'], { relativeTo: this.route });
+  }
 }
